188c4b4029906e95dedf1dafab8fb33eb5f3234e
Errahul15 <errahu532@gmail.com>
2024-03-15 11:26:52 +0530

    Implemented Navbar content and created footer

--- src/app/navbar/nav-content/nav-content.component.ts
+++ src/app/navbar/nav-content/nav-content.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { navigation } from './nav-content';
 
 @Component({
   selector: 'app-nav-content',
@@ -11,7 +12,7 @@ export class NavContentComponent {
 
 
   ngOnInit(){
-    this.category = navigator;
+    this.category = navigation;
   }
 
 }
